929a6079668290339472f554181a9fd5342cb6a4
unknown <shahodatusmonjonova@gmail.com>
2024-06-11 11:49:49 +0500

    inputs

--- src/components/edit-model/EditModel.jsx
+++ src/components/edit-model/EditModel.jsx
@@ -5,19 +5,24 @@ const EditModel = ({ data, setData, onSave, onCancel }) => {
   const nameValue = data ? data.name : "";
   const ageValue = data ? data.age : "";
 
-  const handleInputChange = e => {
-    const { value } = e.target;
-    setData(prev => ({ ...prev, name: value }));
-    setData(prev => ({ ...prev, age: value }));
-  };
+  const handleNameChange = e => {
+  const { value } = e.target;
+  setData(prev => ({ ...prev, name: value }));
+};
+
+const handleAgeChange = e => {
+  const { value } = e.target;
+  setData(prev => ({ ...prev, age: value }));
+};
+
 
   return (
     <>
       <div onClick={() => onCancel()} className="edit__overlay"></div>
       <form className="edit__wrapper">
         <h2>Update user</h2>
-        <input value={nameValue || ''} onChange={handleInputChange} type="text" />
-        <input value={ageValue || ''} onChange={handleInputChange} type="number" />
+        <input value={nameValue || ''} onChange={handleNameChange} type="text" />
+        <input value={ageValue || ''} onChange={handleAgeChange} type="number" />
         <button className='buttons' onClick={() => onSave(data)}>Save</button>
         <button className='buttons' type='button' onClick={() => onCancel()}>Cancel</button>
       </form>
